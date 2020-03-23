c63599cb7c390333515b242ba1bbe66a70fe7eec
tumzied <abdur963rahman@gmil.com>
2020-03-23 14:51:45 +0600

    statuc root add

--- faceapp_api/settings.py
+++ faceapp_api/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
 from .cors_header import  *
